f0a0b9a92fd6723d56df6f58b91fc9be32282ea8
Khairan <alphakhairan@gmail.com>
2021-12-20 19:37:02 +0300

    Project Add

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,27 @@
+using Tools;
+using UnityEngine;
+
+
+internal class CarController : BaseController
+{
+    private readonly ResourcePath _viewPath = new ResourcePath { PathResource = "Prefabs/Car" };
+    private readonly CarView _carView;
+
+    public CarController()
+    {
+        _carView = LoadView();
+    }
+
+    private CarView LoadView()
+    {
+        var objView = Object.Instantiate(ResourceLoader.LoadPrefab(_viewPath));
+        AddGameObjects(objView);
+
+        return objView.GetComponent<CarView>();
+    }
+
+    public GameObject GetViewObject()
+    {
+        return _carView.gameObject;
+    }
+}
